60efb46b39e3dbf0ce96738986e753da86cb01d4
Sachin Yo <sachin_aryal>
2022-09-22 21:43:40 +0545

    Updated css

--- src/components/repositories/repositories.tsx
+++ src/components/repositories/repositories.tsx
@@ -1,7 +1,9 @@
-import styles from './repositories.module.css'
+import style from './repositories.module.css'
 import { useRepositories } from '../../hooks/use-repositories'
 import { useInView } from '../../hooks/use-in-view'
+
 import { useMemo } from 'react'
+import { ClipLoader } from 'react-spinners'
 
 export const Repositories = () => {
   const {
@@ -26,19 +28,29 @@ export const Repositories = () => {
   const isLastRepository = (index: number) => index === totalRepositories - 1
 
   return (
-    <div>
-      <ul className={styles.container}>
+    <div className={style.container}>
+      <h1 className={style.title}>
+        React Infinite scroll demo using React query and Github API
+      </h1>
+      <ul className={style.items}>
         {combinedRepositories.map((combinedRepository, index) => (
           <li
             ref={isLastRepository(index) ? ref : null}
             key={`${combinedRepository.id}-${index}`}
-            className={styles.item}
+            className={style.item}
           >
-            <a href={combinedRepository.html_url}>{combinedRepository.name}</a>
+            <a href={combinedRepository.html_url}>
+              <h4 className={style.heading}>{combinedRepository.full_name}</h4>
+            </a>
+            <p className={style.description}>{combinedRepository.description}</p>
           </li>
         ))}
       </ul>
-      {isFetchingNextPage && <div>Loading...</div>}
+      {isFetchingNextPage && (
+        <div className={style.loader}>
+          <ClipLoader color='#000' size={50} />
+        </div>
+      )}
     </div>
   )
 }
